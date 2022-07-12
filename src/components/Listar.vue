e27ffb8deda4781c10b5122baa3bc029ce54dec1
maurybatalla@gmail.com <maurybatalla@gmail.com>
2022-07-12 11:28:02 -0300

    ss

--- src/components/Listar.vue
+++ src/components/Listar.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="card">
             <div class="card-header">
-                Clientes
+                Clientes Cobranza
             </div>
             <div class="card-body">
                 <div class="form-group">
